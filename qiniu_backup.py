a21af37cfe693f8d16818de0e10de308a2842de0
vfhky <vfhky@qq.com>
2024-09-07 12:23:16 +0800

    七牛增加自动创建bucket

--- qiniu_backup.py
+++ qiniu_backup.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from typing import Dict, Optional
 from logger_wrapper import LoggerWrapper
 from sys_config_entry import SysConfigEntry
-from qiniu import Auth, put_file
+from qiniu import Auth, put_file, BucketManager
 import qiniu.config
 
 class QiniuBackup:
@@ -29,9 +29,26 @@ class QiniuBackup:
         self.dir_name = self.sys_config_entry.get("QINIU_DIR_NAME")
         self.ttl = int(self.sys_config_entry.get("QINIU_EXPIRE_DAYS", 7)) * 24 * 3600
         self.auth = Auth(self.access_key, self.secret_key)
+        self.bucket_manager = BucketManager(self.auth)
+
+    def _ensure_bucket_exists(self):
+        try:
+            buckets, _ = self.bucket_manager.list_bucket()
+            if self.bucket_name not in buckets:
+                ret, info = self.bucket_manager.create_bucket(self.bucket_name, 'z0')
+                if info.status_code == 200:
+                    self.logger.info(f"====> 七牛成功创建 bucket: {self.bucket_name}")
+                else:
+                    self.logger.error(f"====> 七牛创建 bucket 失败: {self.bucket_name}, 错误信息: {info}")
+            else:
+                self.logger.info(f"====> 七牛Bucket 已存在: {self.bucket_name}")
+        except Exception as e:
+            self.logger.error(f"====> 七牛检查或创建 bucket 时出错: {str(e)}")
 
     def backup_dashboard_db(self, db_file: str) -> Optional[str]:
         try:
+            self._ensure_bucket_exists()
+
             now = datetime.now()
             date_prefix = now.strftime(self.DATE_FORMAT)
             month_dir = now.strftime(self.MONTH_FORMAT)
@@ -43,7 +60,6 @@ class QiniuBackup:
             token = self.auth.upload_token(self.bucket_name, upload_path, self.ttl)
             
             ret, info = put_file(token, upload_path, db_file)
-
             if info.status_code == 200:
                 self.logger.info(f"====> 上传到七牛成功 bucket_name={self.bucket_name} {upload_path}")
                 return f"{self.bucket_name}/{upload_path}"
